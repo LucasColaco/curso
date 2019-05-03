7357487670f26b6cda1e7b98790dd43860d6b955
Lucas Colaço <lucascolaco18@gmail.com>
2019-05-03 01:29:46 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/lucascolaco/curso/CursoApplication.java
+++ src/main/java/com/lucascolaco/curso/CursoApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.lucascolaco.curso.domain.Categoria;
+import com.lucascolaco.curso.domain.Produto;
 import com.lucascolaco.curso.repositories.CategoriaRepository;
+import com.lucascolaco.curso.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursoApplication implements CommandLineRunner{ // esse comando vai permitir implementar o metodo auxiliar, para executar alguma acao
 
 	@Autowired // gerar automaticamente
 	private CategoriaRepository categoriaRepository; // dependencia de repositorio
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	
 	public static void main(String[] args) {
@@ -27,8 +31,19 @@ public class CursoApplication implements CommandLineRunner{ // esse comando vai 
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); // salvando os objetos no banco de dados
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
 		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); // salvando os objetos no banco de dados
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		
 
