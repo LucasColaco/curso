e10fb3b6ce9c9157a75b151d388c58055c1ed06f
Lucas Colaço <lucascolaco18@gmail.com>
2019-05-13 16:33:17 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/lucascolaco/curso/CursoApplication.java
+++ src/main/java/com/lucascolaco/curso/CursoApplication.java
@@ -65,6 +65,11 @@ public class CursoApplication implements CommandLineRunner{ // esse comando vai 
 		
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Cama mesa e banho");
+		Categoria cat4 = new Categoria(null, "Eletrônicos");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decoração");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -78,7 +83,7 @@ public class CursoApplication implements CommandLineRunner{ // esse comando vai 
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 	
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); // salvando os objetos no banco de dados
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7)); // salvando os objetos no banco de dados
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais");
